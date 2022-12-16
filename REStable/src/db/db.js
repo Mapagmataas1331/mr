d25140bf859c206bb6114e54f52d4b1a421cbb05
Mapagmataas <mapagmataas@outlook.com>
2022-12-17 04:38:19 +0700

    1331

--- REStable/src/db/db.js
+++ REStable/src/db/db.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/9.15.0/app-check.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCE99ycZW0noggD6NnTaa-tu5FYio0OWpE",
@@ -12,12 +11,6 @@ const firebaseConfig = {
   appId: "1:977282304261:web:180f2b5ef5eaa234900f6c",
   measurementId: "G-MM1VCV4545"
 };
-
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('1323'),
-
-  isTokenAutoRefreshEnabled: true
-});
 
 const app = initializeApp(firebaseConfig);
 
