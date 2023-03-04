59dee5e5f39dbfb99e9b26406d970bb49520cf45
Mapagmataas <mapagmataas@outlook.com>
2023-03-04 23:08:58 +0700

    1331

--- Pixel/src/main.js
+++ Pixel/src/main.js
@@ -6,11 +6,19 @@ const canvas = document.getElementById("canvas");
 const CANVAS_WIGHT = 1600;
 const CANVAS_HEIGHT = 900;
 
-const root = document.querySelector(':root');
-root.style.setProperty('--CANVAS_WIGHT', CANVAS_WIGHT + "px");
-root.style.setProperty('--CANVAS_HEIGHT', CANVAS_HEIGHT + "px");
+// const root = document.querySelector(':root');
+// root.style.setProperty('--CANVAS_WIGHT', CANVAS_WIGHT + "px");
+// root.style.setProperty('--CANVAS_HEIGHT', CANVAS_HEIGHT + "px");
 
 var ctx = canvas.getContext("2d");
+ctx.canvas.width  = CANVAS_WIGHT;
+ctx.canvas.height = CANVAS_HEIGHT;
+const img = new Image();
+img.src = "src/img.jpg";
+
+img.addEventListener("load", () => {
+    ctx.drawImage(img, 0, 0);
+}, false );
 
 canvas.addEventListener('mousedown', function(e) {
     isDown = true;
@@ -18,20 +26,34 @@ canvas.addEventListener('mousedown', function(e) {
         canvas.offsetLeft - e.clientX,
         canvas.offsetTop - e.clientY
     ];
+    document.getElementById("coords").innerHTML =
+    `canvas: ${e.layerX} ${e.layerX}; client: ${e.clientX} ${e.clientY}`;
 }, true);
 
 document.addEventListener('mouseup', function() {
     isDown = false;
 }, true);
 
-document.addEventListener('mousemove', function(event) {
-    event.preventDefault();
+document.addEventListener('mousemove', function(e) {
+    e.preventDefault();
     if (isDown) {
         mousePosition = {
-            x : event.clientX,
-            y : event.clientY
+            x: e.clientX,
+            y: e.clientY
         };
         canvas.style.left = (mousePosition.x + offset[0]) + 'px';
         canvas.style.top  = (mousePosition.y + offset[1]) + 'px';
     }
 }, true);
+
+var zoom = 100;
+canvas.onwheel = function(e) {
+    e.preventDefault();
+    if (e.deltaY < 0) {
+        zoom += 10;
+        canvas.style.zoom = zoom + "%";
+    } else {
+        zoom -= 10;
+        canvas.style.zoom = zoom + "%";
+    };
+};
