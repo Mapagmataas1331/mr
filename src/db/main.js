1e9f4831e4ccad523e92e285483ec3536c295e9f
Mapagmataas <mapagmataas@outlook.com>
2023-04-11 14:02:41 +0700

    1331

--- src/db/main.js
+++ src/db/main.js
@@ -17,6 +17,25 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
+// const { initializeApp } = require('firebase/app');
+// const { getAnalytics } = require('firebase/analytics');
+// const { getDatabase, ref, set, get, update, child } = require('firebase/database');
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCE99ycZW0noggD6NnTaa-tu5FYio0OWpE",
+//   authDomain: "webtest-db.firebaseapp.com",
+//   databaseURL: "https://webtest-db-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "webtest-db",
+//   storageBucket: "webtest-db.appspot.com",
+//   messagingSenderId: "977282304261",
+//   appId: "1:977282304261:web:180f2b5ef5eaa234900f6c",
+//   measurementId: "G-MM1VCV4545"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const db = getDatabase(app);
+
 const fpPromise = import('@fingerprintjs/fingerprintjs')
 .then(FingerprintJS => FingerprintJS.load());
 
