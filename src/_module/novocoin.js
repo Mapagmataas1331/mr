1c04397e6780970156854843ef9b35ea22d8c359
Mapagmataas <mapagmataas@outlook.com>
2023-05-26 02:43:03 +0700

    1331

--- src/_module/novocoin.js
+++ src/_module/novocoin.js
@@ -1,9 +1,3 @@
 trans_arr.push(
   "hey", "хай",
 );
-
-window.onload = () => {
-  setTimeout(() => {
-    document.getElementById("svg").contentDocument.querySelector("svg").style.fill = "var(--primary-text-color)";
-  }, 5000);
-}
