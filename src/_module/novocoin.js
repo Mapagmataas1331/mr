f17689aed1951a6c07b5d8843c9bc9a8dc503fba
Mapagmataas <mapagmataas@outlook.com>
2023-05-29 22:34:02 +0700

    1331

--- src/_module/novocoin.js
+++ src/_module/novocoin.js
@@ -1,3 +1,44 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, get, update, child } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE99ycZW0noggD6NnTaa-tu5FYio0OWpE",
+  authDomain: "webtest-db.firebaseapp.com",
+  databaseURL: "https://webtest-db-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "webtest-db",
+  storageBucket: "webtest-db.appspot.com",
+  messagingSenderId: "977282304261",
+  appId: "1:977282304261:web:180f2b5ef5eaa234900f6c",
+  measurementId: "G-MM1VCV4545"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 trans_arr.push(
   "Transactions:", "Транзакции:",
 );
+
+window.onLogin = () => {
+  const transactions = document.getElementById("transactions");
+  document.getElementById("username").innerHTML = user.name;
+  document.getElementById("amount").innerHTML = user.vid;
+  for (var i = 0; i < 100; i++) {
+    var newEl = document.createElement("p");
+    newEl.className = "transaction";
+    newEl.title = ". . .";
+    newEl.innerHTML = "00.00 00:00 (TEST)<br>you -> Somebody [00]";
+    transactions.appendChild(newEl);
+  }
+}
+
+function updateTransactions() {
+  const transactions = document.getElementById("transactions");
+  for (var i = 0; i < 100; i++) {
+    var newEl = document.createElement("p");
+    newEl.className = "transaction";
+    newEl.title = ". . .";
+    newEl.innerHTML = "00.00 00:00<br>you -> Somebody [00]";
+    transactions.appendChild(newEl);
+  }
+}
