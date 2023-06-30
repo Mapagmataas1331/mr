bea293d3596d9d133fd9a114e0100c80057c5771
Mapagmataas <mapagmataas@outlook.com>
2023-06-30 20:39:48 +0700

    1331

--- src/_module/main.js
+++ src/_module/main.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
 var page = getPage();
--- src/_module/novocoin.js
+++ src/_module/novocoin.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getDatabase(app);
 
 trans_arr.push(
