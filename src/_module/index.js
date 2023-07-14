6b7ed014492d0ef40f3daf01fffcd60b94ad775b
Mapagmataas <mapagmataas@outlook.com>
2023-07-14 22:15:26 +0700

    1331

--- src/_module/index.js
+++ src/_module/index.js
@@ -1,6 +1,6 @@
 trans_arr.push(
-  "Mr_dot is <i class=\"link\">my (Mapagmataas')</i> second website with current projects on my lead",
-  "Мистер_точка это <i class=\"link\">мой (Mapagmataas')</i> второй сайт с проектами которые я сейчас возглавляю",
+  "Mr_dot is <i><a class=\"link\" href=\"https://ma.kak.si/author\">my (author)</a></i> second website with current projects on my lead",
+  "Мистер_точка это <i><a class=\"link\" href=\"https://ma.kak.si/author\">мой (автор)</a></i> второй сайт с проектами которые я сейчас возглавляю",
 );
   
 document.getElementById("imgs").querySelectorAll("img")[0].addEventListener("click", () => {
