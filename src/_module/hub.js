1191bca9f5ff5c42e1a0cc5b0656c0626dbeb349
Mapagmataas <mapagmataas@outlook.com>
2023-09-20 21:52:14 +0700

    1331

--- src/_module/hub.js
+++ src/_module/hub.js
@@ -19,7 +19,7 @@ trans_arr.push(
   "en", "ру",
 );
 
-const canvas = document.getElementById("canvas");
+const bgcanvas = document.getElementById("canvas");
 const CANVAS_WIGHT = 2048;
 const CANVAS_HEIGHT = 1152;
 
@@ -27,9 +27,9 @@ const root = document.querySelector(':root');
 root.style.setProperty('--CANVAS_WIGHT', CANVAS_WIGHT + "px");
 root.style.setProperty('--CANVAS_HEIGHT', CANVAS_HEIGHT + "px");
 
-var ctx = canvas.getContext("2d");
-ctx.canvas.width = CANVAS_WIGHT;
-ctx.canvas.height = CANVAS_HEIGHT;
+var bgctx = bgcanvas.getContext("2d");
+bgctx.canvas.width = CANVAS_WIGHT;
+bgctx.canvas.height = CANVAS_HEIGHT;
 
 var isTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 
@@ -227,31 +227,27 @@ function defFunc(func) {
 }
 
 function checkAfk() {
-  get(ref(database, 'users')).then((snap) => {
+  get(ref(db, 'game/players')).then((snap) => {
     snap.forEach((userSnap) => {
-      if (userSnap.child("game").exists) {
-        if (userSnap.child("game/online").val()) {
-          if (userSnap.child("game/cord_x").val() == userSnap.child("game/cordOld_x").val() && userSnap.child("game/cord_y").val() == userSnap.child("game/cordOld_y").val()) {
-            if (userSnap.child("game/afk_time").val() >= 600) {
-              update(ref(database, `users/${userSnap.key}/game`), {
-                online: false
-              });
-              update(ref(database, `users/${userSnap.key}/game`), {
-                afk_time: 0
-              });
-            } else {
-              update(ref(database, `users/${userSnap.key}/game`), {
-                afk_time: userSnap.child("game/afk_time").val() + 1
-              });
-            }
-          } else {
-            update(ref(database, `users/${userSnap.key}/game`), {
-              cordOld_x: userSnap.child("game/cord_x").val(),
-              cordOld_y: userSnap.child("game/cord_y").val(),
-              afk_time: 0
-            });
-          }
+      if (userSnap.child("cord_x").val() == userSnap.child("cord_x_old").val() && userSnap.child("cord_y").val() == userSnap.child("cord_y_old").val()) {
+        if (userSnap.child("afk_time").val() >= 600) {
+          update(ref(database, `users/${userSnap.key}/game`), {
+            online: false
+          });
+          update(ref(database, `game/players/${userSnap.key}`), {
+            afk_time: 0
+          });
+        } else {
+          update(ref(database, `game/players/${userSnap.key}`), {
+            afk_time: userSnap.child("game/afk_time").val() + 1
+          });
         }
+      } else {
+        update(ref(database, `users/${userSnap.key}/game`), {
+          cord_x_old: userSnap.child("cord_x").val(),
+          cord_y_old: userSnap.child("cord_y").val(),
+          afk_time: 0
+        });
       }
     });
   });
@@ -358,109 +354,109 @@ function checkAfk() {
 var width, height, radius, x_orig, y_orig;
 
 function joystick_init() {
-    joystick = document.getElementById("joystick");
-    joystick.style.display = "block";
-    jsctx = joystick.getContext('2d');
-    resize();
-    document.addEventListener('mousedown', startDrawing);
-    document.addEventListener('mouseup', stopDrawing);
-    document.addEventListener('mousemove', Draw);
-    document.addEventListener('touchstart', startDrawing);
-    document.addEventListener('touchend', stopDrawing);
-    document.addEventListener('touchcancel', stopDrawing);
-    document.addEventListener('touchmove', Draw);
-    window.addEventListener('resize', resize);
+  canvas = document.getElementById("joystick");
+  canvas.style.display = "block";
+  ctx = canvas.getContext('2d');
+  resize();
+  document.addEventListener('mousedown', startDrawing);
+  document.addEventListener('mouseup', stopDrawing);
+  document.addEventListener('mousemove', Draw);
+  document.addEventListener('touchstart', startDrawing);
+  document.addEventListener('touchend', stopDrawing);
+  document.addEventListener('touchcancel', stopDrawing);
+  document.addEventListener('touchmove', Draw);
+  window.addEventListener('resize', resize);
 }
 
 function resize() {
-    width = window.innerWidth;
-    radius = 32;
-    height = radius * 6.5;
-    jsctx.joystick.width = width;
-    jsctx.joystick.height = height;
-    background();
-    joystick(width / 2, height / 3);
+  width = window.innerWidth;
+  radius = 32;
+  height = radius * 6.5;
+  ctx.canvas.width = width;
+  ctx.canvas.height = height;
+  background();
+  joystick(width / 2, height / 3);
 }
 
 function background() {
-    x_orig = width / 2;
-    y_orig = height / 3;
-    jsctx.beginPath();
-    jsctx.arc(x_orig, y_orig, radius + 20, 0, Math.PI * 2, true);
-    jsctx.fillStyle = 'rgba(217, 217, 217, 0.5)';
-    jsctx.fill();
+  x_orig = width / 2;
+  y_orig = height / 3;
+  ctx.beginPath();
+  ctx.arc(x_orig, y_orig, radius + 20, 0, Math.PI * 2, true);
+  ctx.fillStyle = 'rgba(217, 217, 217, 0.5)';
+  ctx.fill();
 }
 
 function joystick(width, height) {
-    jsctx.beginPath();
-    jsctx.arc(width, height, radius, 0, Math.PI * 2, true);
-    jsctx.fillStyle = '#f2f2f2';
-    jsctx.fill();
-    jsctx.strokeStyle = 'rgba(217, 217, 217, 0.5)';
-    jsctx.lineWidth = 8;
-    jsctx.stroke();
+  ctx.beginPath();
+  ctx.arc(width, height, radius, 0, Math.PI * 2, true);
+  ctx.fillStyle = '#f2f2f2';
+  ctx.fill();
+  ctx.strokeStyle = 'rgba(217, 217, 217, 0.5)';
+  ctx.lineWidth = 8;
+  ctx.stroke();
 }
 
 let abs_joy_coord = { x: 0, y: 0 };
 let paint = false;
 
 function getPosition(event) {
-    var mouse_x = event.clientX || event.touches[0].clientX;
-    var mouse_y = event.clientY || event.touches[0].clientY;
-    abs_joy_coord.x = mouse_x - joystick.offsetLeft;
-    abs_joy_coord.y = mouse_y - joystick.offsetTop;
+  var mouse_x = event.clientX || event.touches[0].clientX;
+  var mouse_y = event.clientY || event.touches[0].clientY;
+  abs_joy_coord.x = mouse_x - canvas.offsetLeft;
+  abs_joy_coord.y = mouse_y - canvas.offsetTop;
 }
 
 function is_it_in_the_circle() {
-    var current_radius = Math.sqrt(Math.pow(abs_joy_coord.x - x_orig, 2) + Math.pow(abs_joy_coord.y - y_orig, 2));
-    if (radius >= current_radius) return true
-    else return false
+  var current_radius = Math.sqrt(Math.pow(abs_joy_coord.x - x_orig, 2) + Math.pow(abs_joy_coord.y - y_orig, 2));
+  if (radius >= current_radius) return true
+  else return false
 }
 
 function startDrawing(event) {
-    paint = true;
-    getPosition(event);
-    if (is_it_in_the_circle()) {
-        jsctx.clearRect(0, 0, joystick.width, joystick.height);
-        background();
-        joystick(abs_joy_coord.x, abs_joy_coord.y);
-        Draw(event);
-    }
+  paint = true;
+  getPosition(event);
+  if (is_it_in_the_circle()) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background();
+    joystick(abs_joy_coord.x, abs_joy_coord.y);
+    Draw(event);
+  }
 }
 
 function stopDrawing() {
-    paint = false;
-    jsctx.clearRect(0, 0, joystick.width, joystick.height);
-    background();
-    joystick(width / 2, height / 3);
-    joymap = { x: 0, y: 0};
+  paint = false;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  background();
+  joystick(width / 2, height / 3);
+  joymap = { x: 0, y: 0};
 }
 
 function Draw(event) {
-    if (paint) {
-        jsctx.clearRect(0, 0, joystick.width, joystick.height);
-        background();
-        var x, y;
-        var angle = Math.atan2((abs_joy_coord.y - y_orig), (abs_joy_coord.x - x_orig));
-        if (Math.sign(angle) == -1) {
-            angle_in_degrees = Math.round(-angle * 180 / Math.PI);
-        }
-        else {
-            angle_in_degrees =Math.round( 360 - angle * 180 / Math.PI);
-        }
-        if (is_it_in_the_circle()) {
-            joystick(abs_joy_coord.x, abs_joy_coord.y);
-            x = abs_joy_coord.x;
-            y = abs_joy_coord.y;
-        }
-        else {
-            x = radius * Math.cos(angle) + x_orig;
-            y = radius * Math.sin(angle) + y_orig;
-            joystick(x, y);
-        }
-        getPosition(event);
-        var x_relative = Math.round(x - x_orig);
-        var y_relative = -Math.round(y - y_orig);
-        joymap = { x: x_relative, y: y_relative};
+  if (paint) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background();
+    var x, y;
+    var angle = Math.atan2((abs_joy_coord.y - y_orig), (abs_joy_coord.x - x_orig));
+    if (Math.sign(angle) == -1) {
+      angle_in_degrees = Math.round(-angle * 180 / Math.PI);
     }
-} 
+    else {
+      angle_in_degrees =Math.round( 360 - angle * 180 / Math.PI);
+    }
+    if (is_it_in_the_circle()) {
+      joystick(abs_joy_coord.x, abs_joy_coord.y);
+      x = abs_joy_coord.x;
+      y = abs_joy_coord.y;
+    }
+    else {
+      x = radius * Math.cos(angle) + x_orig;
+      y = radius * Math.sin(angle) + y_orig;
+      joystick(x, y);
+    }
+    getPosition(event);
+    var x_relative = Math.round(x - x_orig);
+    var y_relative = -Math.round(y - y_orig);
+    joymap = { x: x_relative, y: y_relative};
+  }
+}
